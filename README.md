d0cca95f717bb5087aa0349d63c4973503cbe7e0
Adam Richie-Halford <richford@uw.edu>
2018-03-27 20:23:08 -0700

    Turn sgl into a package

--- README.md
+++ README.md
@@ -1,11 +1,4 @@
-[![Documentation Status](https://readthedocs.org/projects/keras_ssg_lasso/badge/?version=latest)](http://keras_ssg_lasso.readthedocs.io/en/latest/?badge=latest)
+# Sparse Group Lasso implementation in `keras`
 
-# Semi-sparse Group Lasso implementation in `keras`
-
-This repository proposes a `keras` implementation of Semi-sparse Group Lasso models for logistic regression
-using the popular `keras` framework.
-
-The file `ex_classif.py` provides an example use of the `SSGL_LogisticRegression` model class.
-
-Directory `xp/` contains code for personal experiments that relies on `tslearn`, but the core of the code only depends 
-on `numpy` and `keras`.
+This repository proposes a `keras` implementation of Sparse Group Lasso models for
+logistic regression and linear regression using the popular `keras` framework.
---
+++ sgl/version.py
@@ -0,0 +1,61 @@
+from __future__ import absolute_import, division, print_function
+
+from os.path import join as pjoin
+
+# Format expected by setup.py and doc/source/conf.py: string of form "X.Y.Z"
+_version_major = 0
+_version_minor = 1
+_version_micro = ''  # use '' for first of series, number for 1 and above
+_version_extra = 'dev'
+# _version_extra = ''  # Uncomment this for full releases
+
+# Construct full version string from these.
+_ver = [_version_major, _version_minor]
+if _version_micro:
+    _ver.append(_version_micro)
+if _version_extra:
+    _ver.append(_version_extra)
+
+__version__ = '.'.join(map(str, _ver))
+
+CLASSIFIERS = ["Development Status :: 3 - Alpha",
+               "Environment :: Console",
+               "Intended Audience :: Science/Research",
+               "License :: OSI Approved :: MIT License",
+               "Operating System :: OS Independent",
+               "Programming Language :: Python",
+               "Topic :: Scientific/Engineering"]
+
+# Description should be a one-liner:
+description = ("keras-sgl: a python library to perform regression with  "
+               "sparse group lasso using the keras library.")
+# Long description will go up on the pypi page
+long_description = """
+keras-sgl
+========
+keras-sgl is a python library designed to perform regression with sparse
+group lasso regularization using the keras library.
+"""
+
+NAME = "keras_sgl"
+MAINTAINER = "Adam Richie-Halford"
+MAINTAINER_EMAIL = "richiehalford@gmail.com"
+DESCRIPTION = description
+LONG_DESCRIPTION = long_description
+URL = "http://github.com/richford/keras_sgl"
+DOWNLOAD_URL = ""
+LICENSE = ""
+AUTHOR = "Adam Richie-Halford"
+AUTHOR_EMAIL = "richiehalford@gmail.com"
+PLATFORMS = "OS Independent"
+MAJOR = _version_major
+MINOR = _version_minor
+MICRO = _version_micro
+VERSION = __version__
+PACKAGE_DATA = {'sgl': [pjoin('data', '*', '*'), ]}
+REQUIRES = [
+    "numpy>=1.11.3", "keras>=2.0", "tensorflow",
+    "sphinx>=1.4",
+]
+EXTRAS_REQUIRE = {}
+ENTRY_POINTS = {}
